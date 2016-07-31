fdca3f3c563ab918dc8ce319c8af0c4fad3ddd71
DuglasAT <avilatorresics@gmail.com>
2016-07-31 09:52:44 -0600

    Fase1 v 0.2

--- Fase1_201314778/analizador.h
+++ Fase1_201314778/analizador.h
@@ -7,19 +7,122 @@
 
 
 
-static char letras [] = {'a','b','c','d','e','f','g',
-                  'h','i','j','k','l','n','m',
-                  'o','p','q','r','s','t','u',
-                  'v','w','x','y','z',
-                  'A','B','C','D','E','F','G',
-                  'H','I','J','K','L','N','O',
-                  'P','M','Q','R','S','T','U'
-                  ,'V','W','X','Y','Z'};
+static int newline = '\n';
+static int sharp = '#';
+static int space = ' ';
+static int caso=0;
+int flag=0;
 
-static char * palabras [] = {"mkdisk","rmdisk","fdisk","-mount","-unmount","-size",
-                             "-unit","-path","-id","-type","-fit","-delete","-name",
-                             "-add","-mkfs","-mkfile","-cat","-rem","-edit","-ren",
-                             "mkdir","-cp","-mv","-find","-cont","-p","-dest","-iddest"};
-static int numeros [] = {'0','1','2','3','4','5','6','7','8','9'};
+
+static char * reservadas [] = {"mkdisk","-size","+unit","-path","-name",
+                               "rmdisk", //-path
+                               "fdisk","+type","+fit","+delete","+add", //-size,+unit,-path,-name
+                               "mount", //-path,-name
+                               "umount","-id",
+
+                               "df","+k","+m","+h","+i",
+                               "du","+n","-h", //-path
+                               "mkfs","+fs", //-id,+type,+add,+unit
+
+                               "login","-usr","-pwd",
+                               "logout",
+                               "mkgrp", //-id,-name
+                               "rmgrp", //-id,-name
+                               "mkusr","-grp", //-id,-usr,-pwd
+                               "rmusr", //-id,-usr
+                               "chmod","ugo","+r", //-id,-path
+                               "mkfile","+p","+size","+cont", //-id,-path
+                               "cat","-file", //-id
+                               "rm","+rf",  //-id,-path
+                               "edit",  //-id,-path,+size,+cont
+                               "ren",  //-id,-path,-name
+                               "mkdir",  //-id,-path,+p
+                               "cp","-dest","-iddest", //-id,-path
+                               "mv",  //-id,-path,-dest,-iddest
+                               "find","?","*","+perm","user",  //-id,-path,-name,-usr
+                               "chown", //-id,-path,+r,-usr
+                               "chgrp", //-usr,-grp
+                               //agregar enlace simbolico ln en fase2
+                               "ulink","-link",
+                               //agregar convert ext2 to ext3 en fase2
+                               "loss", //-ide
+                               "rep", "+ruta", //-name,-path,-id,
+                               "mbr","disk", //para rep
+                               "inode","journaling","block","bm_inode","bm_block","tree","sb","file", //para rep
+                               "ls +i","ls +l", //para rep - verificar en fase2
+                               "exec" //-path
+
+                               };
+
+static int letter(char car)
+{
+
+    int aux = 0;
+    aux = (int)car;
+    if(aux>64 && aux<91 || aux>96 && aux<123)
+    {
+    return 1;
+    }
+
+    return 0;
+}
+
+static int num(char n)
+{
+    int aux = 0;
+    aux = (int)n;
+    if(aux>47 && aux<58)
+    {
+    return 1;
+    }
+
+    return 0;
+}
+
+static void verifica_minuscula(char *cad)
+{
+    char cdobles='\"';
+    while(*cad!='\0')
+    {
+        if(*cad>64 && *cad<91)
+        {
+            if((*cad)==cdobles)
+            {
+                while(*cad==cdobles)
+                {
+                    *cad+=32;
+                    cad++;
+                }
+            }
+            else
+            {
+            *cad+=32;
+            }
+        }
+            if((*cad)==cdobles)
+            {
+            cad++;
+               while(*cad!=cdobles)
+               {
+                   cad++;
+               }
+            }
+        cad++;
+    }
+}
+
+
+void lexer(char buffer [] ){
+
+    int  i =0;
+    int longitud = strlen(&buffer[0]);
+    minusculas(&buffer[0]);
+
+    char dato [1000]={};
+    int pos_vector =0;
+    while (i<tamano){
+
+}
+}
 
 #endif // ANALIZADOR_H_INCLUDED
--- Fase1_201314778/main.c
+++ Fase1_201314778/main.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "analizador.h"
 
 int main()
 {
     printf("Hello world!\n");
+
+    printf("%d\n",letter('a'));
+
+    char buffer [] = "GOLAE";
+
+    verifica_minuscula(&buffer[0]);
+
+    printf("%s\n",buffer);
+
     return 0;
 }
